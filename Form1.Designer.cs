4e4aead8fc0142f1dd61f16fe117fe7c6da121a9
kawarag1 <dima.kretinin.05@mail.ru>
2024-06-26 21:00:24 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace filetest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainText = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.созранитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(367, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Блокнот";
+            // 
+            // MainText
+            // 
+            this.MainText.Location = new System.Drawing.Point(222, 196);
+            this.MainText.Multiline = true;
+            this.MainText.Name = "MainText";
+            this.MainText.Size = new System.Drawing.Size(407, 188);
+            this.MainText.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem,
+            this.созранитьФайлToolStripMenuItem,
+            this.закрытьФайлToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // созранитьФайлToolStripMenuItem
+            // 
+            this.созранитьФайлToolStripMenuItem.Name = "созранитьФайлToolStripMenuItem";
+            this.созранитьФайлToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.созранитьФайлToolStripMenuItem.Text = "Созранить файл";
+            this.созранитьФайлToolStripMenuItem.Click += new System.EventHandler(this.созранитьФайлToolStripMenuItem_Click);
+            // 
+            // закрытьФайлToolStripMenuItem
+            // 
+            this.закрытьФайлToolStripMenuItem.Name = "закрытьФайлToolStripMenuItem";
+            this.закрытьФайлToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.закрытьФайлToolStripMenuItem.Text = "Закрыть файл";
+            this.закрытьФайлToolStripMenuItem.Click += new System.EventHandler(this.закрытьФайлToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Название документа";
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(327, 106);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(177, 20);
+            this.FileName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MainText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MainText;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem созранитьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьФайлToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FileName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace filetest
+{
+    public partial class Form1 : Form
+    {
+        public string selFile = ""; //переменная в которой будет храниться путь к нашему файлу
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog openFileDialog = new OpenFileDialog(); //создаём файловый диалог
+            
+            string filename = "";
+
+
+            if (openFileDialog.ShowDialog(this) != DialogResult.Cancel) //открываем файловый диалог и проверяем его результат
+            {
+                if (openFileDialog.FileName.EndsWith(".txt")) //проверка на расширение выбранного файла
+                {
+                    selFile = openFileDialog.FileName; //сюда записывается путь выбранного файла
+                    filename = openFileDialog.SafeFileName; //сюда записывается название файла
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, выберите файл с расширением \".txt\" ");
+                }
+            }
+
+            try
+            {
+                StreamReader str = new StreamReader(selFile);
+                string txt = str.ReadToEnd(); //переменная txt полностью зачитывает в себя выбранный файл
+                MainText.Text = txt;
+                FileName.Text = filename;
+                str.Close(); //закрываем открытый ранее поток, чтобы мы в дальнейшем смогли изменять файл
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void созранитьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string txt = MainText.Text;
+            try
+            {
+                string name = FileName.Text;
+                if (File.Exists(selFile)) //проверка на существование файла
+                {
+                    StreamWriter str = new StreamWriter(selFile, false);
+                    str.WriteAsync(txt);
+                    str.Close();
+                }
+                else
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog(); //создаём диалог сохранения файла
+                    saveFileDialog.FileName = name;
+                    saveFileDialog.DefaultExt = "txt"; //задаём выходной тип файла
+                    saveFileDialog.Filter = "txt files (*.txt) | .txt"; //задаём фильт по расширению файла
+                    if (saveFileDialog.ShowDialog(this) != DialogResult.Cancel) //открываем файловый диалог и проверяем его результат
+                    {
+                        Stream file = saveFileDialog.OpenFile(); //открываем отдельный поток для сохранения файла
+                        StreamWriter str = new StreamWriter(file); //создаём записывающий поток, в который присоединяем предыдущий отдельно созданный поток
+
+                        str.Write(txt); //полностью записываем текст в файл
+
+                        str.Close();
+                        file.Close();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }    
+        }
+
+        private void закрытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
